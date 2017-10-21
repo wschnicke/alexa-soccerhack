97b64c6897c93e503b6738bf17abfdf318a14944
Stephen Wu <wujstephen@gmail.com>
2017-10-21 15:13:24 -0400

    added console messages for add alias

--- alexa/aliases.py
+++ alexa/aliases.py
@@ -4,10 +4,17 @@ data = {}
 
 def add_alias(alias, team, team_id):
     with codecs.open('data/aliases.json', 'r+', encoding='utf-8') as f:
-        data[alias] = {'id': team_id, 'name': name} # <--- add `id` value.
+        # If found, alert console that it is being replaced
+        if alias in data:
+            print("Replacing alias: " + alias + ": " + json.dumps(data[alias]))
+        # Edit dict
+        data[alias] = {'id': team_id, 'name': team} # <--- add `id` value.
+        # Replace file contents
         f.seek(0)        # <--- should reset file position to the beginning.
         json.dump(data, f, indent=4)
         f.truncate()     # remove remaining part
+        # Alert console
+        print("Added alias: " + alias + ": " + json.dumps(data[alias]))
 
 # Delete alias if it exists, returning value or None
 def remove_alias(alias):
@@ -19,14 +26,16 @@ def get_team(alias):
         return data[alias]
     return None
 
+# Load aliases file
 print("Loading aliases...")
-
 aliases_file = 'data/aliases.json'
 try:
     with codecs.open(aliases_file, 'r+', encoding='utf-8') as f:
         data = json.load(f)
         print("Aliases loaded:")
         print(data)
+        add_alias("Cinci", "FC Cincinatti", 123) # Example alias add
+
 except FileNotFoundError:
     print("Aliases file does not exist...creating it:")
     with open(aliases_file, 'w') as f:

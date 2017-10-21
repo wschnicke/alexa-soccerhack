0f19fb3796447c2b4668355c5edb6f823c508c6e
Stephen Wu <wujstephen@gmail.com>
2017-10-21 14:07:29 -0400

    separated alexa logic and data files

---
+++ alexa/ask.py
@@ -0,0 +1,35 @@
+import logging, configparser
+from flask_ask import Ask, request, session, question, statement
+
+from web.flask import app
+
+ask = Ask(app, "/ask")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_key = config['DEFAULT']['APIkey']
+
+@ask.launch
+def launch():
+    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+@ask.intent('MatchScoreIntent')
+def match_score(team):
+    #TODO: call api to get match score data, set score
+    speech_text = render_template('score', team=team, team_score=team_score, opponent=opponent, opp_score=opp_score)
+    return statement(speech_text)
+
+@ask.intent('HelloWorldIntent')
+def hello_world():
+    speech_text = 'Hello world'
+    return statement(speech_text).simple_card('HelloWorld', speech_text)
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    speech_text = 'You can say hello to me!'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+@ask.session_ended
+def session_ended():
+    return "{}", 200
--- main.py
+++ main.py
@@ -1,45 +1,13 @@
-import logging, os, configparser
+import os
 
 from web.flask import app
 from web.routes import routes
 from web.sockets import socketio
 
-from flask_ask import Ask, request, session, question, statement
+from alexa.ask import ask
 
 # Front-end web logic
 app.register_blueprint(routes)
-
-# Alexa logic
-ask = Ask(app, "/ask")
-logging.getLogger('flask_ask').setLevel(logging.DEBUG)
-config = configparser.ConfigParser()
-config.read('config.ini')
-api_key = config['DEFAULT']['APIkey']
-
-@ask.launch
-def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-
-@ask.intent('MatchScoreIntent')
-def match_score(team):
-    #TODO: call api to get match score data, set score
-    speech_text = render_template('score', team=team, team_score=team_score, opponent=opponent, opp_score=opp_score)
-    return statement(speech_text)
-
-@ask.intent('HelloWorldIntent')
-def hello_world():
-    speech_text = 'Hello world'
-    return statement(speech_text).simple_card('HelloWorld', speech_text)
-
-@ask.intent('AMAZON.HelpIntent')
-def help():
-    speech_text = 'You can say hello to me!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-
-@ask.session_ended
-def session_ended():
-    return "{}", 200
 
 # Main
 if __name__ == '__main__':

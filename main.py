6b78db1ed1d602cc36680c82d66e7eb590ce6df5
gillen58 <pgillen2015@gmail.com>
2017-10-21 13:43:24 -0400

    Added data for MatchScoreIntent

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import logging
 import os
-import configparser
 
 from flask import Flask
 from flask_ask import Ask, request, session, question, statement
-from web.routes import routes
 
-app = Flask(__name__,  template_folder="web/templates", static_folder="web/static")
+from web.routes import routes
+s
+app = Flask(__name__,  template_folder="web")
 
 # Front-end web logic
 app.register_blueprint(routes)
 
 # Alexa logic
-app = Flask(__name__)
 ask = Ask(app, "/ask")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
-config = configparser.ConfigParser()
-api_key = config['DEFAULT']['APIkey']
 
 @ask.launch
 def launch():
     speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
     return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+@ask.intent('MatchScoreIntent')
+def match_score(team):
+    #TODO: call api to get match score data, set score
+    speech_text = render_template('score', team=team, team_score=team_score, opponent=opponent, opp_score=opp_score)
+    return statement(speech_text)
 
 @ask.intent('HelloWorldIntent')
 def hello_world():
@@ -36,6 +39,8 @@ def help():
 @ask.session_ended
 def session_ended():
     return "{}", 200
+
+# Main
 
 if __name__ == '__main__':
     if 'ASK_VERIFY_REQUESTS' in os.environ:

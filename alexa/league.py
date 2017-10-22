04d4d869a1852dc935b1e4be70fe104e9747c222
Will Schnicke <schnicke.4@osu.edu>
2017-10-22 10:25:44 -0400

    fixed league tests

--- alexa/league.py
+++ alexa/league.py
@@ -1,31 +1,31 @@
 import json
 
 # read in list of LIST_OF_LEAGUES
-with open('data/league_list.json')
+with open('data/league_list.json', 'r') as file:
+        leagues = json.load(file)
 
 def get_league(league_name: str):
-"""Returns tuple of [league id (int), proper team name(str)]
-or None if it does not exist
-"""
+    """Returns tuple of [league id (int), proper league name(str)]
+    or None if it does not exist"""
     league_id = None
     if league_name != None:
-        for k, v in teams.items():
+        for k, v in leagues.items():
             if k.lower() == league_name.lower():
                 league_name = k
                 league_id = v;
                 break
 
-    if league_id = None:
-        league_name = None:
+    if league_id == None:
+        league_name = None
     return [league_id, league_name]
 
 def get_league_id(league_name: str):
-"""Returns tuple of [league id (int), proper team name(str)]
-or None if it does not exist
-"""
+    """Returns tuple of [league id (int), proper league name(str)]
+    or None if it does not exist
+    """
     league_id = None
     if league_name != None:
-        for k, v in teams.items():
+        for k, v in leagues.items():
             if k.lower() == league_name.lower():
                 league_id = v;
                 break
